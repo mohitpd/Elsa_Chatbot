261f71875cd70b03ba83068524488cfff81c6253
mohitpd <mohitprasad28@gmail.com>
2020-05-23 14:53:59 +0530

    Train ready

--- Elsa_main.py
+++ Elsa_main.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import random
@@ -11,18 +11,68 @@ import json
 with open("/GitHub/Elsa_Chatbot/intents.json") as file:
     data = json.load(file)
 
-##print(data["intents"])
+
 
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
+
         wrds = nltk.word_tokenize(pattern)
+
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
-        
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+
+for x, doc in enumerate(docs_x):
+
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = np.array(training)
+output = np.array(output)
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
